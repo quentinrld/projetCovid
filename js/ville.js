237560d305898d6f57c1773e731ef8e0fd380237
quentinrld <71373028+quentinrld@users.noreply.github.com>
2021-05-12 10:58:16 +0200

    Create ville.js

---
+++ js/ville.js
@@ -0,0 +1,35 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", initVille);
+
+function initVille(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('get', 'recuperationVille', true);
+    xhr.onload = callbackVilles;
+    xhr.send();
+}
+
+
+function callbackVilles() {
+    let employes = JSON.parse(this.responseText);
+    console.log(employes);
+    let htmlTr = "";
+    for(let employe of employes){
+        htmlTr += "<li>" +
+            employe.codePostal +' '+ employe.villeNom +
+            "</li>";
+    }
+    console.log(htmlTr);
+    document.getElementById("listeVilles").innerHTML = htmlTr;
+    document.getElementById('zoneListeVille').style.display = 'block';
+}
+
+function ajouterVille(formulaireVille) {
+    let xhr = new XMLHttpRequest();
+    let url = 'ajouterVilles?villeNom=(formulaireVille.nomVille.value)&codePostale=(formulaireVille.codePostal.value)';
+    xhr.open('get', 'url', true);
+    xhr.onload = initVille();
+    xhr.send();
+    return false;
+}
+
